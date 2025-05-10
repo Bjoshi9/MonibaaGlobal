51bdc1bb8d3905ae3b825f3ed0a095ab2310086a
Brijesh Joshi <joshi.brijesh.ce@gmail.com>
2025-05-10 23:12:47 +0530

    Updated social details

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 // src/components/Footer.jsx
 import { Link } from 'react-router-dom';
-import { NAVIGATION, COMPANY_INFO } from '../utils/constants';
+import { NAVIGATION, COMPANY_INFO, SOCIAL_MEDIA } from '../utils/constants';
 
 function Footer() {
   return (
@@ -63,12 +63,30 @@ function Footer() {
               Follow us on social media for updates and industry insights.
             </p>
             <div className="flex space-x-4">
-              <a href="#" className="text-gray-300 hover:text-white">
+              {
+                SOCIAL_MEDIA.map((social) => (
+                  <a key={social.name} href={social.url} target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-white transition-colors">
+                    <span className="sr-only">{social.name}</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" x="0px" y="0px" width="24" height="24" viewBox="0 0 50 50">
+                      <path d={social.icon_path}></path>
+                    </svg>
+                  </a>
+                ))
+              }
+              {/* {Object.entries(SOCIAL_MEDIA).map(([key, {name, url, icon }]) => (
+                <a key={key} href='{url}' target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-white transition-colors">
+                  <span className="sr-only">{name}</span>
+                  <svg className={`h-6 w-6 ${icon}`} fill="currentColor" viewBox="0 0 24 24">
+                    <path d={icon} />
+                  </svg>
+                </a>
+              ))} */}
+              {/* <a href="#" className="text-gray-300 hover:text-white">
                 <span className="sr-only">LinkedIn</span>
                 <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
                   <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
                 </svg>
-              </a>
+              </a> */}
             </div>
           </div>
         </div>
